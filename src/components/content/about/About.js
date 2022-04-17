0262fa7f6a697ebc6d2964ab8909b104a655ec7c
prplblck <kariurek@gmail.com>
2022-04-18 01:09:24 +0200

    about start

--- src/components/content/about/About.js
+++ src/components/content/about/About.js
@@ -1,7 +1,63 @@
+import git from './technologies/github.png';
+import react from './technologies/react.png';
+import sass from './technologies/sass.png';
+import parcel from './technologies/parcel.png';
+
+import styles from './about.module.css';
+
 import React from 'react';
 
 const About = () => {
-	return <div>About</div>;
+	return (
+		<div>
+			<header className={styles.header}>
+				<img
+					alt='furry'
+					className={styles.picture}
+					src='https://cdn.weasyl.com/~rowedahelicon/submissions/1864457/b444ac36096ebdfdf6a4eb7be914d686ee8e47ebac75d90438c8519e0450b196/rowedahelicon-pfp-for-awesomeguy117.png'
+				/>
+				<div>
+					<h2>Karol</h2>
+					<p>Frontend Developer</p>
+				</div>
+			</header>
+			<main className={`${styles.grid}`}>
+				<div className={styles.cell}>
+					<h3>Known Technologies</h3>
+					<div
+						className={`${styles.technologies} ${styles.cardInnerPadding}`}
+					>
+						<img src={git} alt='GitHub' />
+						<img src={react} alt='react' />
+						<img src={sass} alt='sass' />
+						<img src={parcel} alt='parcel' />
+					</div>
+				</div>
+				<div className={styles.cell}>
+					<h3>About me</h3>
+					<p className={styles.cardInnerPadding}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Sit voluptatem vitae fuga nihil corrupti optio dolorem,
+						amet temporibus est ad magnam aliquam quod illum fugit
+						numquam et expedita animi impedit.
+					</p>
+				</div>
+				<div className={styles.cell}>
+					<h3>Skills</h3>
+					<ul
+						className={`${styles.skills} ${styles.cardInnerPadding}`}
+					>
+						<li>JavaScript</li>
+						<li>CSS</li>
+						<li>HTML</li>
+						<li>Photoshop</li>
+						<li>English</li>
+						<li>Polish</li>
+					</ul>
+				</div>
+			</main>
+		</div>
+	);
 };
 
 export default About;
--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,4 +1,18 @@
+import styles from './content.module.css';
+
+import { useState } from 'react';
+
+import About from './about/About';
+import Contact from './contact/Contact';
+import Home from './home/Home';
+
 const Content = props => {
-	return <main></main>;
+	const [visibleSection, setVisibleSection] = useState(1);
+
+	const changeVisibleSection = index => setVisibleSection(index);
+
+	const sections = [<Home />, <About />, <Contact />];
+
+	return <main className={styles.padding}>{sections[visibleSection]}</main>;
 };
 export default Content;
