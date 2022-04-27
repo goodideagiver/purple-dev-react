b3d9f4f908d4fd052514c157e94324309163e898
Karol <kariurek@gmail.com>
2022-04-27 12:54:21 +0200

    removed offensive quote

--- src/components/content/home/funnyQuote/FunnyQuote.js
+++ src/components/content/home/funnyQuote/FunnyQuote.js
@@ -5,24 +5,23 @@ import classes from './FunnyQuote.module.css';
 const FunnyQuote = (props) => {
 	return (
 		<div className={classes.funnyQuote}>
+			{/* <p className={classes.comment}>That's about me! 😂</p> */}
 			<blockquote className={classes.blockQuote}>
 				<div className={classes.quoteTextContainer}>
 					<div className={classes.icon}>
 						<RiDoubleQuotesL />
 					</div>
-
-					<p className={classes.quote}>
-						Hire a lazy person to do a difficult job. Because a lazy
-						person will find an easy way to finish the job.
-					</p>
+					<div>
+						<p className={classes.quote}>Unga bunga fire!</p>
+						<p className={classes.author}>
+							- Somebody milion years ago
+						</p>
+					</div>
 					<div className={classes.icon}>
 						<RiDoubleQuotesR />
 					</div>
 				</div>
-
-				<p>- Bill Gates</p>
 			</blockquote>
-			<p className={classes.comment}>That's about me! 😂</p>
 		</div>
 	);
 };
