82b975af65f70789183996ca973625187cb4cc6d
Karol <kariurek@gmail.com>
2022-04-28 10:41:52 +0200

    minor promo.js component refactor

--- src/components/content/home/promo/Promo.js
+++ src/components/content/home/promo/Promo.js
@@ -5,17 +5,15 @@ import st from './promo.module.css';
 import cellData from './data';
 
 const Promo = () => {
-
-
 	return (
 		<div className={st.root}>
 			<ul className={st.list}>
-				{cellData.map((data, id) => (
-					<li key={data.name}>
-						<p className={st.number}>{id + 1}</p>
+				{cellData.map(({ name, desc, number }, id) => (
+					<li key={name}>
+						<p className={number}>{id + 1}</p>
 						<div className={st.cell}>
-							<h3>{data.name}</h3>
-							<p className={st.desc}>{data.desc}</p>
+							<h3>{name}</h3>
+							<p className={st.desc}>{desc}</p>
 						</div>
 					</li>
 				))}
