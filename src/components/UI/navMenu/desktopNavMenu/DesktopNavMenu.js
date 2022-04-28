f35d512357bc0ee57cda7e5302edf854198bb7bd
Karol <kariurek@gmail.com>
2022-04-28 11:23:04 +0200

    refactor  desktop nav menu component

--- src/components/UI/navMenu/desktopNavMenu/DesktopNavMenu.js
+++ src/components/UI/navMenu/desktopNavMenu/DesktopNavMenu.js
@@ -5,38 +5,40 @@ import { MdClose } from 'react-icons/md';
 
 import ReactDOM from 'react-dom';
 import Button from '../../button/Button';
-import { useRef } from 'react';
+
+const Backdrop = ({ backdropClickHandler }) => {
+	return (
+		<div
+			onClick={backdropClickHandler}
+			className={`${styles.backdrop} ${styles['backdrop-show']}`}
+		/>
+	);
+};
+
+const NavContent = ({ onMenuClose }) => {
+	return (
+		<nav className={`${styles.desktopNav} ${styles.show}`}>
+			<Button
+				className={styles.close}
+				onClick={onMenuClose}
+				variant='nav'
+			>
+				<MdClose />
+			</Button>
+			<h2 className={styles.title}>purpleblack.dev</h2>
+			<div className={styles.separator} />
+			<div className={styles.btnContainer}>
+				<NavButtons onClick={onMenuClose} />
+			</div>
+		</nav>
+	);
+};
 
 const DesktopNavMenu = ({ onMenuClose }) => {
-	const backdrop = useRef();
-
-	const backdropClickHandler = (e) => {
-		if (e.target === backdrop.current) {
-			onMenuClose();
-		}
-	};
-
 	return ReactDOM.createPortal(
 		<>
-			<nav className={`${styles.desktopNav} ${styles.show}`}>
-				<Button
-					className={styles.close}
-					onClick={onMenuClose}
-					variant='nav'
-				>
-					<MdClose />
-				</Button>
-				<h2 className={styles.title}>purpleblack.dev</h2>
-				<div className={styles.separator} />
-				<div className={styles.btnContainer}>
-					<NavButtons onClick={onMenuClose} />
-				</div>
-			</nav>
-			<div
-				onClick={backdropClickHandler}
-				ref={backdrop}
-				className={`${styles.backdrop} ${styles['backdrop-show']}`}
-			/>
+			<NavContent onMenuClose={onMenuClose} />
+			<Backdrop backdropClickHandler={onMenuClose} />
 		</>,
 		document.getElementById('overlay-root')
 	);
