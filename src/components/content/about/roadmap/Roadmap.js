1b7234c0f901a63382217e66b66b2768285e4c6b
Karol <kariurek@gmail.com>
2022-04-28 10:28:56 +0200

    moved data from roadmap to separate directory

--- src/components/content/about/roadmap/Roadmap.js
+++ src/components/content/about/roadmap/Roadmap.js
@@ -1,23 +1,15 @@
 import React from 'react';
 
 import timeSince from '../../../../helpers/timeSince';
-
+import goals from './data';
 import styles from './roadmap.module.css';
 
 const Roadmap = ({ className }) => {
-	const goals = [
-		{ name: 'HTML + CSS', date: new Date(2021, 9, 10) },
-		{ name: 'JavaScript', date: new Date(2022, 0, 7) },
-		{ name: 'React', date: new Date(2022, 3, 6) },
-		{ name: 'TypeScript', date: 'TBD' },
-		{ name: 'Next.js', date: 'TBD' },
-	];
-
 	return (
 		<div className={`${className} ${styles.root}`}>
 			<h3>Writing bad code since:</h3>
 			<ul>
-				{goals.map(goal => (
+				{goals.map((goal) => (
 					<li className={styles.item} key={goal.name}>
 						<p className={styles.name}>{goal.name}</p>
 						<p className={styles.date}>
---
+++ src/components/content/about/roadmap/data/index.js
@@ -0,0 +1,9 @@
+const goals = [
+	{ name: 'HTML + CSS', date: new Date(2021, 9, 10) },
+	{ name: 'JavaScript', date: new Date(2022, 0, 7) },
+	{ name: 'React', date: new Date(2022, 3, 6) },
+	{ name: 'TypeScript', date: 'TBD' },
+	{ name: 'Next.js', date: 'TBD' },
+];
+
+export default goals;
