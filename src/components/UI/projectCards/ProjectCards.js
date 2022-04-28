10e3572fc45235fef0a3e37ad36e1bd2ab61b4b3
Karol <kariurek@gmail.com>
2022-04-28 10:56:26 +0200

    moved card data from ProjectCards to separate dir

--- src/components/UI/projectCards/ProjectCards.js
+++ src/components/UI/projectCards/ProjectCards.js
@@ -4,31 +4,9 @@ import styles from './projectCards.module.css';
 
 import ProjectCard from '../projectCard/ProjectCard';
 
-const ProjectCards = () => {
-	const cardData = [
-		{
-			title: 'Fakeinsta',
-			shortDesc: 'Instagram copy',
-			desc: ['OOP', 'Custom Components', 'Async'],
-			link: 'https://github.com/goodideagiver/fakeinsta',
-			color: '#a66fff30',
-		},
-		{
-			title: 'Web app',
-			shortDesc: 'Polite email builder',
-			desc: ['DOM manipulation', 'Vanilla JavaScript'],
-			link: 'https://goodideagiver.github.io/polite-email-builder/',
-			color: '#6284ff21',
-		},
-		{
-			title: 'Adot.pl',
-			shortDesc: 'Digital artist website',
-			desc: ['loremipsum'],
-			link: 'https://www.adot.pl/',
-			color: '#ff00001f',
-		},
-	];
+import cardData from './data';
 
+const ProjectCards = () => {
 	return (
 		<div className={styles.flex}>
 			{cardData.map((card) => (
---
+++ src/components/UI/projectCards/data/index.js
@@ -0,0 +1,25 @@
+const cardData = [
+	{
+		title: 'Fakeinsta',
+		shortDesc: 'Instagram copy',
+		desc: ['OOP', 'Custom Components', 'Async'],
+		link: 'https://github.com/goodideagiver/fakeinsta',
+		color: '#a66fff30',
+	},
+	{
+		title: 'Web app',
+		shortDesc: 'Polite email builder',
+		desc: ['DOM manipulation', 'Vanilla JavaScript'],
+		link: 'https://goodideagiver.github.io/polite-email-builder/',
+		color: '#6284ff21',
+	},
+	{
+		title: 'Adot.pl',
+		shortDesc: 'Digital artist website',
+		desc: ['loremipsum'],
+		link: 'https://www.adot.pl/',
+		color: '#ff00001f',
+	},
+];
+
+export default cardData;
