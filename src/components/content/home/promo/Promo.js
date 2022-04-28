dc0b62b74386083727d6ca64a5d4fcb4023c6c09
Karol <kariurek@gmail.com>
2022-04-28 10:38:40 +0200

    moved data to separate folder for promo component

--- src/components/content/home/promo/Promo.js
+++ src/components/content/home/promo/Promo.js
@@ -2,21 +2,10 @@ import React from 'react';
 
 import st from './promo.module.css';
 
+import cellData from './data';
+
 const Promo = () => {
-	const cellData = [
-		{
-			name: 'Interfaces',
-			desc: 'As user friendly as possible',
-		},
-		{
-			name: 'Websites',
-			desc: 'Easy to use and easy to look at',
-		},
-		{
-			name: 'Visuals',
-			desc: 'Clarity and transparency',
-		},
-	];
+
 
 	return (
 		<div className={st.root}>
---
+++ src/components/content/home/promo/data/index.js
@@ -0,0 +1,16 @@
+const cellData = [
+	{
+		name: 'Interfaces',
+		desc: 'As user friendly as possible',
+	},
+	{
+		name: 'Websites',
+		desc: 'Easy to use and easy to look at',
+	},
+	{
+		name: 'Visuals',
+		desc: 'Clarity and transparency',
+	},
+];
+
+export default cellData
