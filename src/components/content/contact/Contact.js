53a1a0529e0a464d57ce02a21b3e743b5d8e37b0
prplblck <kariurek@gmail.com>
2022-04-30 16:40:23 +0200

    added image to contact section

--- src/components/content/contact/Contact.js
+++ src/components/content/contact/Contact.js
@@ -4,11 +4,21 @@ import ContactOptions from './contactOptions/ContactOptions';
 
 import styles from './contact.module.css';
 
+import code from '../../../assets/img/contact/code.jpg';
+
 const Contact = ({ className }) => {
 	return (
-		<div className={`${styles.root} ${className}`}>
-			<ContactOptions />
-			<ContactForm />
+		<div className={className}>
+			<div className={styles['header-image']}>
+				<h2 className={styles.title}>
+					Choose <span>your</span> way to <span>contact me</span>
+				</h2>
+				<img src={code} alt='' aria-hidden='true' />
+			</div>
+			<div className={`${styles.root} ${className}`}>
+				<ContactOptions />
+				<ContactForm />
+			</div>
 		</div>
 	);
 };
