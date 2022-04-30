33aacaa089786c9feff969eae0fbbb33befc1722
prplblck <kariurek@gmail.com>
2022-04-30 14:26:29 +0200

    plural form logic for timeSince func

--- src/helpers/timeSince.js
+++ src/helpers/timeSince.js
@@ -1,27 +1,30 @@
 //time since date
-const timeSince = date => {
+
+const addPluralFormEnding = (number) => (number > 1 ? 's' : '');
+
+const timeSince = (date) => {
 	const seconds = Math.floor((new Date() - date) / 1000);
 	let interval = Math.floor(seconds / 31536000);
 	if (interval > 1) {
-		return interval + ' years';
+		return interval + ' year' + addPluralFormEnding(interval);
 	}
 	interval = Math.floor(seconds / 2592000);
 	if (interval > 1) {
-		return interval + ' months';
+		return interval + ' month' + addPluralFormEnding(interval);
 	}
 	interval = Math.floor(seconds / 86400);
 	if (interval > 1) {
-		return interval + ' days';
+		return interval + ' day' + addPluralFormEnding(interval);
 	}
 	interval = Math.floor(seconds / 3600);
 	if (interval > 1) {
-		return interval + ' hours';
+		return interval + ' hour' + addPluralFormEnding(interval);
 	}
 	interval = Math.floor(seconds / 60);
 	if (interval > 1) {
-		return interval + ' minutes';
+		return interval + ' minute' + addPluralFormEnding(interval);
 	}
-	return Math.floor(seconds) + ' seconds';
+	return Math.floor(seconds) + ' second' + addPluralFormEnding(interval);
 };
 
 export default timeSince;
