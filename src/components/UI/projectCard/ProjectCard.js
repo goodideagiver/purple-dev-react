204c6666d72a2147677a1fd7b040584058eeca00
Karol <kariurek@gmail.com>
2022-05-19 08:57:03 +0200

    added gradient border for projects

--- src/components/UI/projectCard/ProjectCard.js
+++ src/components/UI/projectCard/ProjectCard.js
@@ -6,26 +6,28 @@ import Button from '../button/Button';
 
 const ProjectCard = ({ title, shortDesc, desc, link, color }) => {
 	return (
-		<div
-			className={styles.card}
-			style={{
-				backgroundImage: `linear-gradient(130deg,transparent 60%, ${color} 100%)`,
-			}}
-		>
-			<p className={styles.subTitle}>{shortDesc}</p>
-			<h3 className={styles.title}>{title}</h3>
-			<ul className={styles.desc}>
-				{desc.map((item) => (
-					<li key={item}>{item}</li>
-				))}
-			</ul>
-			<Button
-				link={link}
-				className={styles.button}
-				variant='callToAction'
+		<div className={styles.cardWrapper}>
+			<div
+				className={styles.card}
+				style={{
+					backgroundImage: `linear-gradient(130deg,transparent 60%, ${color} 100%)`,
+				}}
 			>
-				Check out
-			</Button>
+				<p className={styles.subTitle}>{shortDesc}</p>
+				<h3 className={styles.title}>{title}</h3>
+				<ul className={styles.desc}>
+					{desc.map((item) => (
+						<li key={item}>{item}</li>
+					))}
+				</ul>
+				<Button
+					link={link}
+					className={styles.button}
+					variant='callToAction'
+				>
+					Check out
+				</Button>
+			</div>
 		</div>
 	);
 };
