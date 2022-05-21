b8d55b6517404ad15b06b54a7e7a5733427e35b5
prplblck <kariurek@gmail.com>
2022-05-22 00:48:46 +0200

    added more project cards toggle

--- src/components/UI/projectCards/ProjectCards.js
+++ src/components/UI/projectCards/ProjectCards.js
@@ -1,12 +1,28 @@
-import React from 'react';
-
-import styles from './projectCards.module.css';
-
+import Button from '../button/Button';
 import ProjectCard from '../projectCard/ProjectCard';
-
 import cardData from './data';
+import styles from './projectCards.module.css';
+import { useState } from 'react';
 
 const ProjectCards = () => {
+	const defaultCardOnMainSiteAmount = 3;
+	const [cardOnMainSiteAmount, setCardOnMainSiteAmount] = useState(
+		defaultCardOnMainSiteAmount
+	);
+
+	const toggleAllCardsVisibility = () => {
+		setCardOnMainSiteAmount(
+			cardOnMainSiteAmount === defaultCardOnMainSiteAmount
+				? Infinity
+				: defaultCardOnMainSiteAmount
+		);
+	};
+
+	const toggleButtonText =
+		cardOnMainSiteAmount === defaultCardOnMainSiteAmount
+			? 'Show more projects'
+			: 'Show less projects';
+
 	return (
 		<>
 			<div className={styles.flex}>
@@ -18,10 +34,18 @@ const ProjectCards = () => {
 						see more
 					</p>
 				</div>
-				{cardData.map((card) => (
-					<ProjectCard {...card} key={card.title} />
-				))}
+				{cardData.map(
+					(card, index) =>
+						index < cardOnMainSiteAmount && (
+							<ProjectCard {...card} key={card.title} />
+						)
+				)}
 			</div>
+			{cardData.length > defaultCardOnMainSiteAmount && (
+				<Button variant='secondary' onClick={toggleAllCardsVisibility}>
+					{toggleButtonText}
+				</Button>
+			)}
 		</>
 	);
 };
--- src/components/UI/projectCards/data/index.js
+++ src/components/UI/projectCards/data/index.js
@@ -20,6 +20,27 @@ const cardData = [
 		link: 'https://www.adot.pl/',
 		color: '#6276D720',
 	},
+	{
+		title: 'purpleblack.dev',
+		shortDesc: 'This website',
+		desc: ['REACT', 'ROUTER'],
+		link: 'https://github.com/goodideagiver/purple-dev-react',
+		color: '#6276D720',
+	},
+	{
+		title: 'CLICK THE BUTTON',
+		shortDesc: 'Mini fun project',
+		desc: ['Vanilla JavaScript', 'Clean code', 'Error handling'],
+		link: 'https://github.com/goodideagiver/click-the-button',
+		color: '#6276D720',
+	},
+	{
+		title: 'THE DOGAPP',
+		shortDesc: 'React UI project',
+		desc: ['React', 'Redux', 'Conditional UI', 'API'],
+		link: 'https://github.com/goodideagiver/the-dogapp',
+		color: '#6276D720',
+	},
 ];
 
 export default cardData;
