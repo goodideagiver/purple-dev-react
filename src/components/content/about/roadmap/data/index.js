b5ddcb6243f983c6e2655c03d0deec21b7e485be
Karol <kariurek@gmail.com>
2022-06-13 14:19:44 +0200

    Add TS start date

--- src/components/content/about/roadmap/data/index.js
+++ src/components/content/about/roadmap/data/index.js
@@ -2,7 +2,7 @@ const goals = [
 	{ name: 'HTML + CSS', date: new Date(2021, 9, 10) },
 	{ name: 'JavaScript', date: new Date(2022, 0, 7) },
 	{ name: 'React', date: new Date(2022, 3, 6) },
-	{ name: 'TypeScript', date: 'TBD' },
+	{ name: 'TypeScript', date: new Date(2022, 5, 13) },
 	{ name: 'Next.js', date: 'TBD' },
 ];
 
