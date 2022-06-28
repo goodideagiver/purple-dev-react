0fefb37c215eb0aece2d55fef18c84bb5c9c9fca
Karol <kariurek@gmail.com>
2022-06-28 14:02:49 +0200

    Remove github commits - last commit date

--- src/components/UI/projectCard/ProjectCard.js
+++ src/components/UI/projectCard/ProjectCard.js
@@ -1,8 +1,5 @@
-import React, { useEffect } from 'react';
-
 import CardBtn from './components/CardBtn';
 import styles from './projectCard.module.css';
-import { useState } from 'react';
 
 const ProjectCardFeaturesList = (props) => (
 	<ul className={styles.desc}>
@@ -19,27 +16,8 @@ const ProjectCard = ({
 	link,
 	color,
 	delay = 0,
-	commitsFetchUrl,
 	tooltip = 'View repository',
 }) => {
-	const [lastCommitDate, setLastCommitDate] = useState(null);
-
-	useEffect(() => {
-		if (commitsFetchUrl) {
-			fetch(commitsFetchUrl + '?per_page=1')
-				.then((response) => response.json())
-				.then((data) => {
-					console.log(data);
-					const lastCommitDate = new Date(data[0].commit.author.date);
-					setLastCommitDate(
-						lastCommitDate.toLocaleDateString() +
-							' ' +
-							lastCommitDate.toLocaleTimeString()
-					);
-				});
-		}
-	}, [commitsFetchUrl]);
-
 	const cardGradientBg = {
 		backgroundImage: `linear-gradient(130deg,transparent 60%, ${color} 100%)`,
 	};
@@ -50,9 +28,6 @@ const ProjectCard = ({
 		<div style={cardRevealDelay} className={styles.cardWrapper}>
 			<div className={styles.card} style={cardGradientBg}>
 				<p className={styles.subTitle}>{shortDesc}</p>
-				{lastCommitDate && (
-					<p className={styles.commit}>Last commit: {lastCommitDate}</p>
-				)}
 				<h3 className={styles.title}>{title}</h3>
 				<ProjectCardFeaturesList desc={desc} />
 				<CardBtn link={link} tooltip={tooltip} />
