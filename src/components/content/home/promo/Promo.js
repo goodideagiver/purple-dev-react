82bb7ff705300e5f6cd2b3e07ba5ef9db934610b
Karol <kariurek@gmail.com>
2022-06-28 15:37:49 +0200

    Add awesome waves effect to promo

--- src/components/content/home/promo/Promo.js
+++ src/components/content/home/promo/Promo.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 import st from './promo.module.css';
+import classes from './wave.module.css';
 
 import cellData from './data';
 
 const Promo = () => {
+	const listElements = cellData.map(({ name, desc, number }, id) => (
+		<li key={name}>
+			<p className={st.number}>{id + 1}</p>
+			<div className={st.cell}>
+				<h3>{name}</h3>
+				<p className={st.desc}>{desc}</p>
+			</div>
+		</li>
+	));
+
 	return (
-		<div className={st.root}>
-			<ul className={st.list}>
-				{cellData.map(({ name, desc, number }, id) => (
-					<li key={name}>
-						<p className={st.number}>{id + 1}</p>
-						<div className={st.cell}>
-							<h3>{name}</h3>
-							<p className={st.desc}>{desc}</p>
-						</div>
-					</li>
-				))}
-			</ul>
+		<div className={`${st.root} ${classes.wave}`}>
+			<ul className={st.list}>{listElements}</ul>
+			<div className={classes.waveFirst}></div>
+			<div className={classes.waveSecond}></div>
+			<div className={classes.waveThird}></div>
 		</div>
 	);
 };
