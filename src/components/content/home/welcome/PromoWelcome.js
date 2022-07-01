9f7b8bf69ffb54a43d7224220a6d85c91f22a841
prplblck <kariurek@gmail.com>
2022-07-01 19:33:29 +0200

    Typo

--- src/components/content/home/welcome/PromoWelcome.js
+++ src/components/content/home/welcome/PromoWelcome.js
@@ -7,7 +7,7 @@ export const PromoWelcome = () => (
 			Aspiring frontend <span className={st.devText}>developer</span>
 		</p>
 		<p className={st.info}>
-			This is my website, here you can learn some usefull information about me.
+			This is my website, here you can learn some useful information about me.
 			I am always trying to learn new things and I am always trying to improve
 			my skills.
 		</p>
