d2a9975ade91ecce94d915eb7d5a2b7bda691af8
prplblck <kariurek@gmail.com>
2022-07-30 11:29:22 +0200

    Change welcome text

--- src/components/content/home/welcome/PromoWelcome.js
+++ src/components/content/home/welcome/PromoWelcome.js
@@ -4,12 +4,12 @@ import st from './PromoWelcome.module.css';
 export const PromoWelcome = () => (
 	<div className={st.promo}>
 		<p className={st.bigText}>
-			Aspiring frontend <span className={st.devText}>developer</span>
+			Frontend <span className={st.devText}>developer</span>
 		</p>
 		<p className={st.info}>
-			This is my website, here you can learn some useful information about me.
-			I am always trying to learn new things and I am always trying to improve
-			my skills.
+			This is my website, here you can learn some useful information about me. I
+			am always trying to learn new things and I am always trying to improve my
+			skills.
 		</p>
 		<Button route='/contact' className={st.button} variant='glow'>
 			Contact me
