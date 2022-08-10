88ddf542e55cc2e41ee7b1aec51b767f42f9f90a
prplblck <kariurek@gmail.com>
2022-08-10 23:13:33 +0200

    Change text on welcome image

--- src/components/content/home/welcome/PromoWelcome.js
+++ src/components/content/home/welcome/PromoWelcome.js
@@ -7,9 +7,9 @@ export const PromoWelcome = () => (
 			Frontend <span className={st.devText}>developer</span>
 		</p>
 		<p className={st.info}>
-			This is my website, here you can learn some useful information about me. I
-			am always trying to learn new things and I am always trying to improve my
-			skills.
+			Welcome to my website, here you can learn some useful information about
+			me. I am always trying to learn new things and I am always trying to
+			improve my skills.
 		</p>
 		<Button route='/contact' className={st.button} variant='glow'>
 			Contact me
