382b682ba446cf7ada80a43ce0d2cdfc33bf44a8
prplblck <kariurek@gmail.com>
2022-08-15 10:05:30 +0200

    Add analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,28 @@ import { Content } from '../src/components/content/Content';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Content>
-			<NavTop />
-			<Component {...pageProps} />
-			<Footer />
-		</Content>
+		<>
+			<script
+				strategy='lazyOnload'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+
+			<script strategy='lazyOnload'>
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+			</script>
+			<Content>
+				<NavTop />
+				<Component {...pageProps} />
+				<Footer />
+			</Content>
+		</>
 	);
 }
 
