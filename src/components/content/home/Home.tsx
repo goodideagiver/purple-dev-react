69a8b3d763d71b1d01568386d76b8b9754c9e532
prplblck <kariurek@gmail.com>
2022-08-15 16:11:30 +0200

    Refactor home

--- src/components/content/home/Home.tsx
+++ src/components/content/home/Home.tsx
@@ -4,22 +4,41 @@ import Welcome from './welcome/Welcome';
 import Promo from './promo/Promo';
 import AboutMeCTA from '../../UI/aboutMeCTA/AboutMeCTA';
 import FunnyQuote from './funnyQuote/FunnyQuote';
+import { ReactNode } from 'react';
+
+const ContactCTASection = () => (
+	<>
+		<FunnyQuote />
+		<div className={st.filler} />
+		<AboutMeCTA />
+	</>
+);
+
+type HomeComponent = {
+	component: ReactNode;
+	css?: string;
+	sectionWidth?: boolean;
+};
+
+const HomeComponents: HomeComponent[] = [
+	{ component: <Welcome />, css: st.homeAnim },
+	{ component: <Promo />, sectionWidth: false },
+	{ component: <ProjectCards />, css: st.nextTo },
+	{ component: <ContactCTASection />, css: st.nextTo },
+];
 
 export const Home = () => {
-	return (
-		<>
-			<section className={`${st.sectionWidth} ${st.homeAnim}`}>
-				<Welcome />
+	const HomeComponentsWithSection = HomeComponents.map((component) => {
+		return (
+			<section
+				className={`${
+					component.sectionWidth === false ? '' : st.sectionWidth
+				} ${component.css ? component.css : ''}`}
+			>
+				{component.component}
 			</section>
-			<Promo />
-			<section className={`${st.nextTo} ${st.sectionWidth}`}>
-				<ProjectCards />
-			</section>
-			<section className={`${st.nextTo} ${st.sectionWidth}`}>
-				<FunnyQuote />
-				<div className={st.filler} />
-				<AboutMeCTA />
-			</section>
-		</>
-	);
+		);
+	});
+
+	return <>{HomeComponentsWithSection}</>;
 };
