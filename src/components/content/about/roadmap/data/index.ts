4fddbb5dc00b43bc7b7074a6f4a30b101aeee921
Karol <kariurek@gmail.com>
2022-08-16 13:15:48 +0200

    Refactor goals

--- src/components/content/about/roadmap/data/index.ts
+++ src/components/content/about/roadmap/data/index.ts
@@ -1,4 +1,6 @@
-const goals = [
+type Goal = { name: String; date: Date | 'TBD' };
+
+const goals: Goal[] = [
 	{ name: 'HTML + CSS', date: new Date(2021, 9, 10) },
 	{ name: 'JavaScript', date: new Date(2022, 0, 7) },
 	{ name: 'React', date: new Date(2022, 3, 6) },
