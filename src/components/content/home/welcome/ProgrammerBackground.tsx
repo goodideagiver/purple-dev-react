6bb69de470d755bd9a350624a5bafb9a6ddfb10f
prplblck <kariurek@gmail.com>
2022-10-02 19:56:35 +0200

    Feat: add parallax effect for promo bg programmer image

--- src/components/content/home/welcome/ProgrammerBackground.tsx
+++ src/components/content/home/welcome/ProgrammerBackground.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
 import ImgSmoothLoad from '../../../UI/ImgSmoothLoad/ImgSmoothLoad';
 import img from '/public/assets/img/home/forest.webp';
-import st from './welcome.module.css';
+import { ScrollParallax } from 'react-just-parallax';
+
+import classes from './ProgrammerBackground.module.css';
 
 export const ProgrammerBackground = () => {
 	return (
-		<div className={st.programmer} aria-hidden='true'>
-			<div className={`${st.programmer} ${st.overlay}`} />
-			<ImgSmoothLoad
-				priority={true}
-				className={st.image}
-				src={img}
-				alt=''
-				aria-hidden='true'
-			/>
+		<div className={classes.programmer} aria-hidden='true'>
+			<ScrollParallax>
+				<div className={`${classes.programmer} ${classes.overlay}`} />
+				<ImgSmoothLoad
+					priority={true}
+					className={classes.image}
+					src={img}
+					alt=''
+					aria-hidden='true'
+				/>
+			</ScrollParallax>
 		</div>
 	);
 };
