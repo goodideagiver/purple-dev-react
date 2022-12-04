d69a0c4d477bbfbdf66cbd5549cf6dd65a297928
prplblck <kariurek@gmail.com>
2022-12-04 22:59:44 +0100

    Fix: remove eslint error

--- src/components/StrapiError/StrapiError.tsx
+++ src/components/StrapiError/StrapiError.tsx
@@ -5,7 +5,7 @@ export const StrapiError = () => {
     <div className={classes.root}>
       <main className={classes.info}>
         <h2>Something went wrong...</h2>
-        <p>I probably didn't have time to migrate strapi from heroku</p>
+        <p>I probably didn{`'`}t have time to migrate strapi from heroku</p>
       </main>
     </div>
   )
