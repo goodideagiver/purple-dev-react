88dac1aada482a25b21099de5561e7505bf0ea75
Karol <kariurek@gmail.com>
2023-06-07 13:56:58 +0200

    Add bewebdev project

--- src/components/UI/projectCards/data/articles/ArticleTools/ArticleTemplate/ArticleTemplate.tsx
+++ src/components/UI/projectCards/data/articles/ArticleTools/ArticleTemplate/ArticleTemplate.tsx
@@ -1,79 +1,79 @@
-import { Gallery } from '../Gallery';
-import { ArticleSection } from './ArticleSection';
-import { ArticleTechnologies } from './ArticleTechnologies';
+import { Gallery } from '../Gallery'
+import { ArticleSection } from './ArticleSection'
+import { ArticleTechnologies } from './ArticleTechnologies'
 
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
 
-import classes from './ArticleTemplate.module.css';
-import { Button } from '../../../../../button/Button';
-import { Image } from '../GalleryImage.types';
-import { ReactNode } from 'react';
+import classes from './ArticleTemplate.module.css'
+import { Button } from '../../../../../button/Button'
+import { Image } from '../GalleryImage.types'
+import { ReactNode } from 'react'
 
 const container = {
-	hidden: { opacity: 1, scale: 0 },
-	visible: {
-		opacity: 1,
-		scale: 1,
-		transition: {
-			delayChildren: 0.35,
-			staggerChildren: 0.2,
-		},
-	},
-};
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.35,
+      staggerChildren: 0.2,
+    },
+  },
+}
 
 type ArticleTemplateProps = {
-	technologies: string[];
-	images: Image[];
-	features: ReactNode;
-	whatDidILearn: ReactNode;
-	demoLink: string;
-	repoLink: string;
-};
+  technologies: string[]
+  images?: Image[]
+  features?: ReactNode
+  whatDidILearn: ReactNode
+  demoLink: string
+  repoLink: string
+}
 
 export const ArticleTemplate = ({
-	technologies,
-	features,
-	whatDidILearn,
-	repoLink,
-	demoLink,
-	images,
+  technologies,
+  features,
+  whatDidILearn,
+  repoLink,
+  demoLink,
+  images,
 }: ArticleTemplateProps) => {
-	return (
-		<motion.div
-			variants={container}
-			initial='hidden'
-			animate='visible'
-			className={classes.root}
-		>
-			{images && <Gallery images={images} />}
-			{whatDidILearn && (
-				<ArticleSection sectionName='🧠 What did I learn'>
-					{whatDidILearn}
-				</ArticleSection>
-			)}
-			{features && (
-				<ArticleSection sectionName='✨ Features'>{features}</ArticleSection>
-			)}
-			{technologies && <ArticleTechnologies technologiesArray={technologies} />}
-			{(!!repoLink || !!demoLink) && (
-				<section className={classes.links}>
-					{repoLink && (
-						<Button
-							variant='callToAction'
-							href={repoLink}
-							purpose='externalLink'
-						>
-							{' '}
-							📦 Repo
-						</Button>
-					)}{' '}
-					{demoLink && (
-						<Button variant='glow' purpose='externalLink' href={demoLink}>
-							🎬 Demo
-						</Button>
-					)}
-				</section>
-			)}
-		</motion.div>
-	);
-};
+  return (
+    <motion.div
+      variants={container}
+      initial='hidden'
+      animate='visible'
+      className={classes.root}
+    >
+      {images && <Gallery images={images} />}
+      {whatDidILearn && (
+        <ArticleSection sectionName='🧠 What did I learn'>
+          {whatDidILearn}
+        </ArticleSection>
+      )}
+      {features && (
+        <ArticleSection sectionName='✨ Features'>{features}</ArticleSection>
+      )}
+      {technologies && <ArticleTechnologies technologiesArray={technologies} />}
+      {(!!repoLink || !!demoLink) && (
+        <section className={classes.links}>
+          {repoLink && (
+            <Button
+              variant='callToAction'
+              href={repoLink}
+              purpose='externalLink'
+            >
+              {' '}
+              📦 Repo
+            </Button>
+          )}{' '}
+          {demoLink && (
+            <Button variant='glow' purpose='externalLink' href={demoLink}>
+              🌐 Website
+            </Button>
+          )}
+        </section>
+      )}
+    </motion.div>
+  )
+}
